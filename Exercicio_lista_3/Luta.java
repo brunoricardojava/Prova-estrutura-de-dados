b5fbefec66c0ca09042e4e2edac9a026965d8633
Bruno Ricardo <brunoricardojava@gmail.com>
2016-02-10 13:45:02 -0200

    Atualização

--- Exercicio_lista_3/Luta.java
+++ Exercicio_lista_3/Luta.java
@@ -1,7 +1,16 @@
+import javax.swing.JOptionPane;
+
 public class Luta{
   public static void main(String args[]) {
 
-    Warrior guerreiro = new blueWarrior("Bruno","Guerreiro",200,200,300);
+  	String Nome = JOptionPane.showInputDialog("Digite o nome do guerreiro:");
+  	String Tipo = JOptionPane.showInputDialog("Digite o tipo do guerreiro:");
+  	int Vida = Integer.parseInt(JOptionPane.showInputDialog("Digite a vida do guerreiro:"));
+  	int Ataque = Integer.parseInt(JOptionPane.showInputDialog("Digite o ataque do guerreiro:"));
+  	int Defesa = Integer.parseInt(JOptionPane.showInputDialog("Digite a defesa do guerreiro:"));
+
+
+    Warrior guerreiro = new blueWarrior(Nome,Tipo,Vida,Ataque,Defesa);
     Monster monstro = new zumbi("Kevin","Monstro",10,20,50);
 
     guerreiro.mostraInfo();
